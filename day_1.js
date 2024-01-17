a5c7ea751ca6b9bdf0718acb9cb65b5c16b7ff96
Shimu Mallick <404.zerobug@gmail.com>
2024-01-17 21:23:16 +0600

    1 day 5 problem solving done

---
+++ day_1.js
@@ -0,0 +1,74 @@
+// 1. তোমাকে নীচের লাইন গুলো যে ফরম্যাট এ আছে একইভাবে প্রিন্ট করতে হবে-
+
+// “Recently I heard that you’ve achieved 95% marks in your exam.
+// This is brilliant!
+// I wish you’ll shine in your life!	Good luck with all the barriers(/\) in your life.”
+
+// const str = "Recently I heard that you’ve achieved 95% marks in your exam. \nThis is brilliant! \nI wish you’ll shine in your life!	Good luck with all the barriers(/\\) in your life."
+
+// console.log(str);
+
+// 2. তোমাকে দুইটি ইন্টিজার ভ্যেলু ইনপুট নিতে হবে এবং তাদের যোগফল, বিয়োগফল, গুণফল এবং ভাগফল নীচে দেখানো ফরম্যাট এ দেখাতে হবে।
+
+// For example:
+// Inputs are 5 and 2
+// Then you’ll give output as:
+// 5 + 2 = 7
+// 5 - 2 = 3
+// 5 * 2 = 10
+// 5 / 2 = 2.50
+
+// const num1 = 10;
+
+// const num2 = 5;
+
+// const sum = num1 + num2;
+
+// const sub = num1 - num2;
+
+// const mul = num1 * num2;
+
+// const div = num1 / num2;
+
+// console.log("10 + 5 = ", sum);
+// console.log("10 - 5 = ", sub);
+// console.log("10 * 5 = ", mul);
+// console.log("10 / 5 = ", div);
+
+// 3. তোমাকে একটি অঋণাত্মক (০ বা এর থেকে বড়) সংখ্যা নিতে হবে এবং বলতে হবে সংখ্যাটি জোড়(even) নাকি বিজোড়(odd)
+
+// const num = 10;
+
+// if (num % 2 == 0) {
+//     console.log("Even Number");
+// } else {
+//     console.log("Odd Number");
+// }
+
+// 4. তোমাকে একটি ইন্টিজার ভ্যেলু ইনপুট নিতে হবে এবং বলতে হবে এটি ধনাত্মক(positive) নাকি ঋণাত্মক(negative) নাকি শূণ্য(zero)।
+
+// const num = -10;
+
+// if (num == 0) {
+//     console.log("zero");
+// } else if (num > 0) {
+//     console.log("positive");
+// } else if (num < 0) {
+//     console.log("negative");
+// }
+
+// 5. ধরো তোমার এক বন্ধু তোমাকে বলেছে সে একটি Gucci Bag কিনবে যদি তার কাছে ১০ হাজার টাকা বা এর বেশি থাকে। নাহলে যদি তার কাছে ৫ হাজার বা এর বেশি টাকা থাকে তাহলে সে Levis Bag কিনবে। আর নাহলে সে নিউ মার্কেট থেকে কিছু (Something) কিনে নিবে। সে তোমাকে আরও বললো যে, যদি সে Gucci Bag কিনতে পারে এবং তার কাছে যদি ২০ হাজার টাকা এর বেশি থাকে তাহলে সে আরও একটি Gucci Belt কিনবে।
+// এখন ধরো তুমি জানো তোমার বন্ধুর কাছে কত টাকা আছে, তুমি কি বলতে পারবে সে কোন কোন জিনিস গুলো কিনবে?
+
+// const money = 20000;
+
+// if (money >= 10000) {
+//   console.log("Gucci Bag");
+//   if (money >= 20000) {
+//     console.log("Gucci Belt");
+//   }
+// } else if (money >= 5000) {
+//   console.log("Levis Bag");
+// } else {
+//   console.log("Something");
+// }
